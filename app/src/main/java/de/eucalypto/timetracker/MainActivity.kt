704b6ec3c09c818f466e56894e5da220ceb818d0
Viktor Papara <eucalypto@mail.de>
2020-08-26 16:39:49 +0200

    Set up custom mime type for the intent filter
    Now the filter should work without interfering with other apps filters like text/plain

--- app/src/main/java/de/eucalypto/timetracker/MainActivity.kt
+++ app/src/main/java/de/eucalypto/timetracker/MainActivity.kt
@@ -7,17 +7,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import de.eucalypto.timetracker.databinding.ActivityMainBinding
-import java.nio.charset.StandardCharsets
-import kotlin.experimental.and
 
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        const val MIME_TEXT_PLAIN = "text/plain"
-    }
-
-    //    private val nfcAdpter: NfcAdapter = NfcAdapter.getDefaultAdapter(applicationContext)
     private lateinit var binding: ActivityMainBinding
 
 
@@ -41,24 +34,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun handleIntent(intent: Intent) {
-        val action = intent.action
-        val type = intent.type
 
         val extra = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES) ?: return
 
         val message = extra.first() as NdefMessage
 
         val payload = message.records.first().payload
-        val textEncoding = if (payload[0] and 128.toByte() == 0.toByte())
-            StandardCharsets.UTF_8 else StandardCharsets.UTF_16
 
-        val languageCodeLength = (payload[0] and 63.toByte()).toInt() + 1
-        binding.text.text = String(
-            payload,
-            languageCodeLength,
-            payload.size - languageCodeLength,
-            textEncoding
-        )
+        binding.text.text = String(payload)
 
     }
 
