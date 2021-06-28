7332925bfbb15895b406a43e5a33f4ec0893e8f1
Viktor Papara <developer@eucalypto.net>
2021-06-28 15:56:14 +0200

    Write NFC tag successfully
    #4

---
+++ app/src/main/java/net/eucalypto/timetracker/writenfc/TagHandler.kt
@@ -0,0 +1,20 @@
+package net.eucalypto.timetracker.writenfc
+
+import android.nfc.NdefMessage
+import android.nfc.Tag
+import android.nfc.tech.Ndef
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class TagHandler(tag: Tag) {
+
+    private val ndef: Ndef = Ndef.get(tag)
+
+    suspend fun writeMessage(message: NdefMessage) = withContext(Dispatchers.IO) {
+        ndef.use { ndef ->
+            ndef.connect()
+            if (ndef.isConnected)
+                ndef.writeNdefMessage(message)
+        }
+    }
+}
--- app/src/main/java/net/eucalypto/timetracker/writenfc/WriteNfcActivity.kt
+++ app/src/main/java/net/eucalypto/timetracker/writenfc/WriteNfcActivity.kt
@@ -9,16 +9,18 @@ import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.nfc.tech.NfcA
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import net.eucalypto.timetracker.databinding.WriteNfcActivityBinding
+import timber.log.Timber
+import java.io.IOException
 
 class WriteNfcActivity : AppCompatActivity() {
 
     private lateinit var adapter: NfcAdapter
-
-    private lateinit var pendingIntent: PendingIntent
-    private lateinit var intentFiltersArray: Array<IntentFilter>
-    private lateinit var techListArray: Array<Array<String>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,40 +28,49 @@ class WriteNfcActivity : AppCompatActivity() {
         val binding = WriteNfcActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val fooIntent = Intent(this, this::class.java).apply {
-            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-        }
-        pendingIntent = PendingIntent.getActivity(this, 0, fooIntent, 0)
-
-        val ndefIntentFilter = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED).apply {
-            try {
-                addDataType("*/*")
-            } catch (e: IntentFilter.MalformedMimeTypeException) {
-                throw RuntimeException("fail", e)
-            }
-        }
-        intentFiltersArray = arrayOf(ndefIntentFilter)
-        techListArray = arrayOf(arrayOf(NfcA::class.java.name))
-
-
         adapter = NfcAdapter.getDefaultAdapter(this)
+    }
 
-        val record = NdefRecord.createMime("text/plain", "This is dog".toByteArray())
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+
+        val record = NdefRecord.createMime("text/plain", "IT is me, your future self".toByteArray())
         val androidApplicationRecord =
             NdefRecord.createApplicationRecord(this.packageName)
         val message = NdefMessage(record, androidApplicationRecord)
 
 
-    }
-
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
         val tagFromIntent: Tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)!!
-        val foo = 42
+        val tag = TagHandler(tagFromIntent)
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                tag.writeMessage(message)
+                Toast
+                    .makeText(this@WriteNfcActivity, "Tag written successfully", Toast.LENGTH_LONG)
+                    .show()
+            } catch (e: IOException) {
+                Timber.e(e, "Could not write NFC tag")
+                Toast.makeText(this@WriteNfcActivity, "Error writing tag", Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 
     override fun onResume() {
         super.onResume()
+
+        val fooIntent = Intent(this, this::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, fooIntent, 0)
+
+        val ndefIntentFilter = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED).apply {
+            addDataType("*/*")
+        }
+        val intentFiltersArray = arrayOf(ndefIntentFilter)
+        val techListArray = arrayOf(arrayOf(NfcA::class.java.name))
+
+
         adapter.enableForegroundDispatch(
             this,
             pendingIntent,
