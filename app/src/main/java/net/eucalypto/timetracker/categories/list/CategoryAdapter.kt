b5f4cb7eac0f435172a6da3a4ce3420a62e1841d
Viktor Papara <developer@eucalypto.net>
2021-07-06 20:01:03 +0200

    Restructure Category List: context menu
    Now the actions to manage a category are not available as buttons in the list, but as menu items in the context menu that can be opened by a) clicking on the "menu button" with the 4 vertical dots, or b) by long pressing the category list item.
    
    #7

--- app/src/main/java/net/eucalypto/timetracker/categories/list/CategoryAdapter.kt
+++ app/src/main/java/net/eucalypto/timetracker/categories/list/CategoryAdapter.kt
@@ -1,11 +1,14 @@
 package net.eucalypto.timetracker.categories.list
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.DiffUtil.ItemCallback
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import net.eucalypto.timetracker.R
 import net.eucalypto.timetracker.databinding.CategoryListItemBinding
 import net.eucalypto.timetracker.domain.model.Category
 
@@ -31,6 +34,7 @@ private constructor(private val binding: CategoryListItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     lateinit var category: Category
+    private val context = binding.root.context
 
     fun bind(
         category: Category,
@@ -39,8 +43,37 @@ private constructor(private val binding: CategoryListItemBinding) :
     ) {
         this.category = category
         binding.categoryName.text = category.name
-        binding.writeNfcButton.setOnClickListener { onWriteNfcButtonClicked(category) }
-        binding.editButton.setOnClickListener { onEditButtonClicked(category) }
+        binding.contextMenuButton.setOnClickListener {
+            showContextMenu(it, onEditButtonClicked, onWriteNfcButtonClicked)
+        }
+        binding.root.setOnLongClickListener {
+            showContextMenu(binding.contextMenuButton, onEditButtonClicked, onWriteNfcButtonClicked)
+            true
+        }
+    }
+
+    private fun showContextMenu(
+        it: View,
+        onEditButtonClicked: (Category) -> Unit,
+        onWriteNfcButtonClicked: (Category) -> Unit
+    ) {
+        val menu = PopupMenu(context, it)
+        menu.inflate(R.menu.category_list_item_menu)
+        menu.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_item_edit -> {
+                    onEditButtonClicked(category)
+                }
+                R.id.menu_item_delete -> {
+
+                }
+                R.id.menu_item_write_nfc -> {
+                    onWriteNfcButtonClicked(category)
+                }
+            }
+            true
+        }
+        menu.show()
     }
 
     companion object {
